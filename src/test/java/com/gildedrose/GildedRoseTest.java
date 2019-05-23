da32b38e8f5cca9f25f872d0f16d7680a365abb2
Christian Fischer <cf@fisch3r.de>
2019-05-23 12:54:35 +0200

    add further tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,26 +4,51 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Gilded Rose")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class GildedRoseTest {
+class GildedRoseTest {
 
     @Test
-    public void reduces_Item_Quality_on_Update() {
-        Item[] items = new Item[] { new Item("item", 5, 8) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
+    void reduces_Item_Quality_on_Update() {
+        Item[] items = singleItemList(5, 8);
+        updateItems(items);
         assertThat(items[0].quality).isEqualTo(7);
     }
 
+
+    @ParameterizedTest
+    @ValueSource(ints = { 0,-1 })
+    void reduces_Item_Quality_twice_as_fast_after_sell_by_date(int sellIn) {
+        Item[] items = singleItemList(sellIn, 10);
+        updateItems(items);
+        assertThat(items[0].quality).isEqualTo(8);
+    }
+
     @Test
-    public void reduces_Item_Sell_In_on_Update() {
-        Item[] items = new Item[] { new Item("item", 5, 7) };
+    void reduces_Item_Sell_In_on_Update() {
+        Item[] items = singleItemList(5, 7);
+        updateItems(items);
+        assertThat(items[0].sellIn).isEqualTo(4);
+    }
+
+    @Test
+    void never_reduces_Item_Quality_below_zero() {
+        Item[] items = singleItemList(5, 0);
+        updateItems(items);
+        assertThat(items[0].quality).isEqualTo(0);
+    }
+
+    private Item[] singleItemList(int sellIn, int quality) {
+        return new Item[]{new Item("", sellIn, quality)};
+    }
+
+    private void updateItems(Item[] items) {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertThat(items[0].sellIn).isEqualTo(4);
     }
 }
---
+++ src/test/java/com/gildedrose/SpecialItemsUpdateSpecification.java
@@ -0,0 +1,36 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Gilded Rose")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class SpecialItemsUpdateSpecification {
+
+    @Test
+    void increases_Quality_Of_Brie_on_Update() {
+        Item[] items = singleItemList("Aged Brie",5,10);
+        updateItems(items);
+        assertThat(items[0].quality).isEqualTo(11);
+    }
+
+    @Test
+    void increases_Quality_Of_Brie_twice_as_fast_after_sell_by_date() {
+        Item[] items = singleItemList("Aged Brie",0,10);
+        updateItems(items);
+        assertThat(items[0].quality).isEqualTo(12);
+    }
+
+    private Item[] singleItemList(String item, int sellIn, int quality) {
+        return new Item[]{new Item(item, sellIn, quality)};
+    }
+
+    private void updateItems(Item[] items) {
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+    }
+}

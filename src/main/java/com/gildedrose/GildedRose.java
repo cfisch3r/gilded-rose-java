3a2811fc2fa614b5b83148f09de5d44d0768174b
Christian Fischer <cf@fisch3r.de>
2019-05-23 16:15:28 +0200

    refactoring

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,7 +20,7 @@ class GildedRose {
                     items[i].quality = items[i].quality + 1;
                 }
 
-                } else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            } else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
@@ -45,19 +45,17 @@ class GildedRose {
             } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
 
 
-                } else {
+            } else {
                     if (items[i].quality > 0) {
                         items[i].quality = items[i].quality - 1;
                     }
 
                     items[i].sellIn = items[i].sellIn - 1;
 
-                if (items[i].sellIn < 0)
+                if (items[i].quality > 0 && items[i].sellIn < 0)
                     items[i].quality = items[i].quality - 1;
 
-                }
-
-
+            }
 
         }
     }

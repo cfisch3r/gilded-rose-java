3ee79b2fbd131fc6d6405269dfca32fe55d8e1f1
Christian Fischer <cf@fisch3r.de>
2019-06-09 12:08:52 +0200

    fix ci pipeline

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
-git config user.email "ci@agiledojo.de"
+openssl aes-256-cbc -K $encrypted_716a67714777_key -iv $encrypted_716a67714777_iv -in key.enc -out key -d
+chmod 600 key
+eval $(ssh-agent -s)
+
+ssh-add keygit config user.email "ci@agiledojo.de"
 git config user.name "CI"
 gh-pages --dist build/reports/jgiven/test/html -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git

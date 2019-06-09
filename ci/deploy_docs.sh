645e14fe31cb8fe2034fc2c5b02231aea5130679
Christian Fischer <cf@fisch3r.de>
2019-06-09 12:20:46 +0200

    refactor ci build

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 
+# setup ssh key for git access from encrypted file
 openssl aes-256-cbc -K $encrypted_716a67714777_key -iv $encrypted_716a67714777_iv -in key.enc -out key -d
 chmod 600 key
 eval $(ssh-agent -s)
 ssh-add key
 
+# push to gh pages
 git config user.email "ci@agiledojo.de"
 git config user.name "CI"
 gh-pages --dist build/reports/jgiven/test/html -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
--- ci/setup_ssh_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-openssl aes-256-cbc -K $encrypted_716a67714777_key -iv $encrypted_716a67714777_iv -in key.enc -out key -d
-chmod 600 key
-eval $(ssh-agent -s)
-ssh-add key

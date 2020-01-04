54b1cf7210d5c346267bd52d73b636b8c459449f
CI <ci@agiledojo.de>
2020-01-04 09:07:08 +0000

    Updates

--- data/metaData.js
+++ data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"Jun 10, 2019, 2:20:41 PM","version":"0.17.1-9cea246233f6","title":"JGiven Report","data":["data0.js"],"showThumbnails":true} );
+jgivenReport.setMetaData({"created":"Jan 4, 2020, 9:07:01 AM","version":"0.17.1-9cea246233f6","title":"JGiven Report","data":["data0.js"],"showThumbnails":true} );
